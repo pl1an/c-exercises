32c394b69b03ada5bc5cb3398748ad3593979035
Ian Paleta <159561711+pl1an@users.noreply.github.com>
2024-02-27 21:40:23 -0300

    Add files via upload

---
+++ pt1/ex8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    float r;
+
+    scanf("%f", &r);
+
+    // you could also declare a area variable and print it instead.
+    // float area;
+    // area = r*r*3.14159;
+    // printf("%f", area);
+    printf("%f", raio*raio*3.14159);
+
+    return 0;
+}
